620fa54fc2d743c78229b5267379ff917c16ca30
mithrandyr <mithrandyr@users.noreply.github.com>
2022-11-19 15:00:42 -0500

    Added Script to construct module and place SQLite files into platform specific folders and load them according\ Fixed 78

--- source/ConstructModule.ps1
+++ source/ConstructModule.ps1
@@ -8,7 +8,7 @@ if(Test-Path $outputFolder) { Remove-Item -Path $outputFolder -Force -Recurse}
 New-Item -Path $outputFolder -ItemType Directory | Out-Null
 New-Item -Path $binFolder -ItemType Directory | Out-Null
 
-Get-ChildItem -Path $engineFolder -Filter *.dll -File |
+Get-ChildItem -Path $engineFolder -Filter *.dll -File -Recurse |
 	Copy-Item -Destination $binFolder 
 
 Get-ChildItem -Path $cmdletFolder -Filter *.dll -File |
@@ -16,5 +16,22 @@ Get-ChildItem -Path $cmdletFolder -Filter *.dll -File |
 	Copy-Item -Destination $outputFolder
 	
 Set-Location $PSScriptRoot
+
+#Process SQLite Interops
+$Platforms = @("linux-x64", "osx-x64", "win-x64", "win-x86")
+$Version = Get-Item "Output\bin\System.Data.SQLite.dll" | Select-Object -ExpandProperty VersionInfo | Select-Object -ExpandProperty FileVersion
+$Version = $Version.Substring(0, $Version.Length - 2)
+$SQLitePath = "$home\.nuget\packages\system.data.sqlite.core\$version\runtimes"
+$SQLiteDll = Join-Path $binFolder "System.Data.SQLite.dll"
+
+foreach ($p in $Platforms) {
+	$dstPath = Join-Path $binFolder $p	
+	$srcPath = Join-Path $SQLitePath "$p\native\netstandard2.0\SQLite.Interop.dll"
+	
+	New-Item -ItemType Directory -Path $dstPath | Out-Null
+	Copy-Item -Path $srcPath -Destination $dstPath #interop File
+	Copy-Item -Path $SQLiteDll -Destination $dstPath
+}
+Remove-Item -Path $SQLiteDll
 
 Import-Module .\Output\SimplySql.Cmdlets.dll -Verbose
--- source/SimplySql.Cmdlets/ContextHandling.vb
+++ source/SimplySql.Cmdlets/ContextHandling.vb
@@ -1,5 +1,6 @@
 Imports System.Reflection
 Imports System.IO
+Imports System.Runtime.InteropServices
 Public Class ContextHandling
     Implements IModuleAssemblyInitializer, IModuleAssemblyCleanup
 
@@ -17,17 +18,30 @@ Public Class ContextHandling
 
     Private Shared Function HandleResolveEvent(ByVal sender As Object, ByVal args As ResolveEventArgs) As Assembly
         Dim asmName = New AssemblyName(args.Name)
-
-        If asmName.Name.Equals("SimplySql.Engine") Then
+        Dim asmPath As String
+        If asmName.Name.Equals("SimplySql.Engine", StringComparison.OrdinalIgnoreCase) Then
             IsEngineLoaded = True
             Return Assembly.LoadFile(Path.Combine(BinPath, "SimplySql.Engine.dll"))
         End If
 
-        If IsEngineLoaded AndAlso AssemblyList.Contains(asmName.Name) Then
-            Dim asmPath As String = Path.Combine(BinPath, $"{asmName.Name}.dll")
-            Return Assembly.LoadFile(asmPath)
+        If IsEngineLoaded Then
+            If asmName.Name.Equals("System.Data.SQLite", StringComparison.OrdinalIgnoreCase) Then
+                If RuntimeInformation.IsOSPlatform(OSPlatform.OSX) Then
+                    asmPath = Path.Combine(BinPath, $"osx-x64\{asmName.Name}.dll")
+                ElseIf RuntimeInformation.IsOSPlatform(OSPlatform.Linux) Then
+                    asmPath = Path.Combine(BinPath, $"linux-x64\{asmName.Name}.dll")
+                Else
+                    If Environment.Is64BitProcess Then
+                        asmPath = Path.Combine(BinPath, $"win-x64\{asmName.Name}.dll")
+                    Else
+                        asmPath = Path.Combine(BinPath, $"win-x86\{asmName.Name}.dll")
+                    End If
+                End If
+            ElseIf AssemblyList.Contains(asmName.Name) Then
+                asmPath = Path.Combine(BinPath, $"{asmName.Name}.dll")
+            End If
+            If Not String.IsNullOrWhiteSpace(asmPath) Then Return Assembly.LoadFile(asmPath)
         End If
-
         Return Nothing
     End Function
 End Class
