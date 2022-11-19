9fd3ff826fd31add44519206e5ec7476f8bb7672
mithrandyr <mithrandyr@users.noreply.github.com>
2022-11-19 14:58:57 -0500

    Fixed #77 Reverted to System.Data for MSSQL and SQLite. Added Open-SQLiteConnection Started SQLite Provider

--- source/SimplySql.Engine/SQLiteProvider.vb
+++ source/SimplySql.Engine/SQLiteProvider.vb
@@ -1,7 +1,8 @@
-﻿Public Class SQLiteProvider
+﻿Imports System.Data.SQLite
+Public Class SQLiteProvider
     Inherits ProviderBase
 
-    Public Sub New(connectionName As String, commandTimeout As Integer, connection As Microsoft.Data.Sqlite.SqliteConnection)
+    Public Sub New(connectionName As String, commandTimeout As Integer, connection As SQLiteConnection)
         MyBase.New(connectionName, Common.ProviderTypes.SQLite)
     End Sub
     Public Overrides Sub ChangeDatabase()
@@ -20,7 +21,22 @@
         Throw New NotImplementedException()
     End Function
 
-    Public Shared Function Create() As SQLiteProvider
-        Return Nothing
+    Public Shared Function Create(connectionName As String, dataSource As String, password As String, commandTimeout As Integer) As SQLiteProvider
+        Dim sb As New SQLiteConnectionStringBuilder
+        If Not dataSource.Equals(":memory:", StringComparison.OrdinalIgnoreCase) Then
+            Dim filepath = New IO.FileInfo(dataSource)
+            If Not filepath.Directory.Exists Then filepath.Directory.Create()
+        End If
+
+        sb.DataSource = dataSource
+
+        If Not String.IsNullOrWhiteSpace(password) Then sb.Password = password
+
+        Return Create(connectionName, sb.ToString, commandTimeout)
+    End Function
+    Public Shared Function Create(connectionName As String, connectionString As String, commandTimeout As Integer) As SQLiteProvider
+        Dim conn As New SQLiteConnection(connectionString)
+        conn.Open()
+        Return New SQLiteProvider(connectionName, commandTimeout, conn)
     End Function
 End Class
