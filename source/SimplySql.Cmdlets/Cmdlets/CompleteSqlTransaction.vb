e3ddb65977ec723f2fcf7759869cacdc69e00853
mithrandyr <mithrandyr@users.noreply.github.com>
2022-12-01 10:42:58 -0500

    fixes #83

---
+++ source/SimplySql.Cmdlets/Cmdlets/CompleteSqlTransaction.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Management.Automation.Language
+
+<Cmdlet(VerbsLifecycle.Complete, "SqlTransaction", SupportsShouldProcess:=True)>
+Public Class CompleteSqlTransaction
+    Inherits PSCmdlet
+#Region "Parameters"
+    <Parameter(ValueFromPipelineByPropertyName:=True)>
+    <[Alias]("cn")>
+    <ValidateNotNullOrEmpty()>
+    Public Property ConnectionName As String = "default"
+#End Region
+
+    Protected Overrides Sub ProcessRecord()
+        If Not Engine.Logic.ConnectionExists(ConnectionName) Then
+            ErrorConnectionNotFound(ConnectionName)
+        Else
+            If Me.ShouldProcess(ConnectionName, "Commit a Sql Transaction") Then
+                Try
+                    Engine.Logic.GetConnection(ConnectionName).CommitTransaction()
+                Catch ex As Exception
+                    ErrorOperationFailed(ex, ConnectionName)
+                End Try
+            End If
+        End If
+    End Sub
+End Class
