9c85c93ea1182c9297af031b0ca8abf23320e908
mithrandyr <mithrandyr@users.noreply.github.com>
2022-12-06 14:13:31 -0500

    cleaning up repo

--- BinarySrc/SqlMessage.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class SqlMessage
-    Public ReadOnly Property Received As DateTime
-    Public ReadOnly Property Message As String
-
-    Public Sub New(_received As DateTime, _message As String)
-        Received = _received
-        Message = _message
-    End Sub
-End Class
