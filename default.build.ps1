5aa326095d7ba8acd6645ff8070edb499da4e5a9
mithrandyr <mithrandyr@users.noreply.github.com>
2022-12-09 12:49:26 -0500

    Updating Build Process

--- default.build.ps1
+++ default.build.ps1
@@ -1,56 +1,37 @@
-task Build {
-  remove "source\output"
-  exec {
-    dotnet publish source -c release -o "source\output"
+New-Alias -Name HV -Value (Resolve-Path HandleVerbose.ps1)
+
+task Clean { remove "output" }
+task Build { Invoke-Build -File "source\source.build.ps1" } 
+
+task ComposeModule {
+  if(-not (Test-Path "output\SimplySql" -PathType Container)) {
+    New-Item "Output\SimplySql" -ItemType Directory | Out-Null
   }
-} 
-
-task Clean {
-  remove "source\output"
-  foreach($prj in @("SimplySql.Cmdlets","SimplySql.Common","SimplySql.Engine","SQLite")) {
-    remove "source\$prj\bin" -verbose
-    remove "source\$prj\obj" -verbose
-  }
-}
-
-task CreateModule {
-  remove "Output\SimplySql"
-  New-Item "Output\SimplySql" -ItemType Directory | Out-Null
-  #Base Module Files
-  $baseFiles = @(
-    "SimplySql\SimplySql.psd1"
-    "Source\Output\SimplySql.Cmdlets.dll"
-    "Source\Output\EnumerableToDataReader.dll"
-  ) | Copy-Item -Destination "Output\SimplySql" -PassThru
-
-  #Copy files for engine
-  New-Item "Output\SimplySql\bin" -ItemType Directory | Out-Null
-  Get-ChildItem "Source\Output\*.dll" -Exclude $baseFiles.Name |
-    Copy-Item -Destination "Output\SimplySql\bin"
-}
+}, copyManifest, copyBinaries, GenerateDocs
 
 task GenerateDocs {
-
+  Start-Job -ScriptBlock {
+        Set-Location $using:BuildRoot
+        Import-Module ".\output\SimplySql"
+        
+        if(-not (Test-Path "docs")) { New-MarkdownHelp -Module SimplySql -OutputFolder Docs }
+        else { Update-MarkdownHelp -Path "Docs" }
+      } |
+    Receive-Job -Wait -AutoRemoveJob |
+    Select-Object -Expand Name |
+    HV "Generating Module Documentation" "."
 }
 
-task SQLite_Interops {
-  $envList = "win-x64","win-x86","linux-x64","osx-x64"
-
-  foreach($env in $envlist) {
-    remove "source\output"
-    exec {
-      dotnet publish source\SQLite -c release -o "source\output" -r $env
-    }
-    
-    $dest = "output\SimplySql\bin\$env"
-    remove $dest
-    New-Item $dest -ItemType Directory | Out-Null
-    Get-ChildItem "source\output\*.dll" -exclude "SQLite.dll" |
-      Copy-Item -Destination $dest    
-  }
-  remove "output\SimplySql\bin\System.Data.SQLite.dll"
+task copyManifest {
+  #Base Module Files
+  Copy-Item "ModuleManifest\SimplySql.psd1" -Destination "output\SimplySql" -Force
+}
+task copyBinaries {
+  #Copy files for engine
+  Copy-Item "source\output\*" -Destination "output\SimplySql" -Force
+  Copy-Item "source\output\bin" -Destination "output\SimplySql" -Recurse -Force  
 }
 
 
-task . Build, CreateModule, SQLite_Interops, Clean, GenerateDocs
-task Debugging Build
+
+task . Build, ComposeModule
--- source/source.build.ps1
+++ source/source.build.ps1
@@ -1,11 +1,17 @@
 param([switch]$DebugOnly)
+
+if(-not [bool](Get-ChildItem alias:\).where({$_.name -eq "hv"})) {
+    New-Alias -Name HV -Value (Resolve-Path ..\HandleVerbose.ps1)
+}
+
 task Clean { remove output }
 
 task Build {
     $configuration = "Release"
     if($DebugOnly) { $configuration = "debug"}
     
-    exec { dotnet publish "SimplySql.Cmdlets" -c $Configuration -o "output\bin" }
+    exec { dotnet publish "SimplySql.Cmdlets" -c $Configuration -o "output\bin" } | HV "Building SimplySql.Cmdlets" "."
+    
     Move-Item "output\bin\SimplySql.Cmdlets.dll" -Destination "output"
     Move-Item "output\bin\EnumerableToDataReader.dll" -Destination "output"
     remove "output\bin\System.Data.SQLite.dll"
@@ -13,7 +19,7 @@ task Build {
     $envList = @("win-x64")
     if(-not $DebugOnly) { $envList += @("win-x86", "linux-x64", "osx-x64")}
     foreach($env in $envList) {
-        exec { dotnet publish SQLite -c $Configuration -r $env -o "output\bin\$env"}
+        exec { dotnet publish SQLite -c $Configuration -r $env -o "output\bin\$env"} | HV "Building SQLite (Interop) for $env" "."
         Remove-Item "output\bin\$env" -Exclude "SQLite.Interop.dll", "System.Data.SQLite.dll" -Recurse
     }
 }
