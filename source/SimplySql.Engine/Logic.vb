fbf5bb2f3b4b38bef58648f8c7dc40bfe1e28f03
mithrandyr <mithrandyr@users.noreply.github.com>
2024-05-11 17:21:44 -0400

    refactored provider creation logic

--- source/SimplySql.Engine/Logic.vb
+++ source/SimplySql.Engine/Logic.vb
@@ -1,4 +1,6 @@
-﻿Public Module Logic
+﻿Imports System.Management.Automation
+
+Public Module Logic
     ReadOnly Property Connections As New Dictionary(Of String, ISimplySqlProvider)
 
     Function ConnectionExists(connectionName As String, Optional checkIsOpen As Boolean = True) As ValidateConnectionResult
@@ -28,15 +30,17 @@
         Dim provider As ISimplySqlProvider
         Select Case connDetail.ConnectionType
             Case ProviderTypes.MSSQL
-                provider = MSSQLProvider.Create(connDetail)
+                provider = CreateProviderMSSQL(connDetail)
             Case ProviderTypes.MySql
-                provider = MySqlProvider.Create(connDetail)
+                provider = CreateProviderMySQL(connDetail)
             Case ProviderTypes.Oracle
-                provider = OracleProvider.Create(connDetail)
+                provider = CreateProviderOracle(connDetail)
             Case ProviderTypes.PostGre
-                provider = PostGreProvider.Create(connDetail)
+                provider = CreateProviderPostGre(connDetail)
             Case ProviderTypes.SQLite
-                provider = SQLiteProvider.Create(connDetail)
+                provider = CreateProviderSQLite(connDetail)
+            Case Else
+                Throw New ArgumentOutOfRangeException(NameOf(connDetail), connDetail.ConnectionType, $"'{connDetail.ConnectionType}' is not a supported provider.")
         End Select
         provider.Connection.Open()
         Connections.Add(connDetail.ConnectionName, provider)
@@ -62,4 +66,22 @@
             Throw New KeyNotFoundException($"No connection named '{connectionName}' exists.")
         End Try
     End Sub
+
+#Region "Provider Create Functions"
+    Private Function CreateProviderMSSQL(connDetail As baseConnectionDetail) As ISimplySqlProvider
+        Return MSSQLProvider.Create(connDetail)
+    End Function
+    Private Function CreateProviderMySql(connDetail As baseConnectionDetail) As ISimplySqlProvider
+        Return MySqlProvider.Create(connDetail)
+    End Function
+    Private Function CreateProviderOracle(connDetail As baseConnectionDetail) As ISimplySqlProvider
+        Return OracleProvider.Create(connDetail)
+    End Function
+    Private Function CreateProviderPostGre(connDetail As baseConnectionDetail) As ISimplySqlProvider
+        Return PostGreProvider.Create(connDetail)
+    End Function
+    Private Function CreateProviderSQLite(connDetail As baseConnectionDetail) As ISimplySqlProvider
+        Return SQLiteProvider.Create(connDetail)
+    End Function
+#End Region
 End Module
